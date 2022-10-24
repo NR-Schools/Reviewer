d14f51237d81840ca0f3d9505d6dd19fd9d17336
Null-Root <triascend94@gmail.com>
2022-10-24 10:18:51 +0800

    add: mnemonics and key strokes

--- src/MenuStuffs.java
+++ src/MenuStuffs.java
@@ -1,10 +1,12 @@
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.ButtonGroup;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -13,6 +15,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JSeparator;
+import javax.swing.KeyStroke;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -28,7 +31,7 @@ public class MenuStuffs extends JFrame {
     
     JMenuBar menubar;
     
-    JMenu reg_items, rb_items, cm_1, cm_2, cm_3;
+    JMenu reg_items, rb_items, cb_items, cm_1, cm_2, cm_3;
     JPopupMenu popMenu;
     
     JSeparator sep;
@@ -37,6 +40,8 @@ public class MenuStuffs extends JFrame {
     
     JRadioButtonMenuItem rb_item_1, rb_item_2, rb_item_3;
     ButtonGroup bg;
+    
+    JCheckBoxMenuItem cb_item_1, cb_item_2;
     
     public MenuStuffs() {
         super();
@@ -47,6 +52,7 @@ public class MenuStuffs extends JFrame {
         
         reg_items = new JMenu("Normal");
         rb_items = new JMenu("RB");
+        cb_items = new JMenu("CB");
         popMenu = new JPopupMenu();
         
         cm_1 = new JMenu("cm1");
@@ -56,6 +62,14 @@ public class MenuStuffs extends JFrame {
         
         sep = new JSeparator();
         reg_item_1 = new JMenuItem("reg1");
+        reg_item_1.setMnemonic(KeyEvent.VK_R);
+        reg_item_1.setAccelerator(
+                KeyStroke.getKeyStroke(
+                        KeyEvent.VK_R,
+                        ActionEvent.CTRL_MASK
+                )
+        );
+        
         reg_item_2 = new JMenuItem("reg2");
         
         pop_item_1 = new JMenuItem("pop1");
@@ -64,6 +78,9 @@ public class MenuStuffs extends JFrame {
         rb_item_1 = new JRadioButtonMenuItem("rb1");
         rb_item_2 = new JRadioButtonMenuItem("rb2");
         rb_item_3 = new JRadioButtonMenuItem("rb3");
+        
+        cb_item_1 = new JCheckBoxMenuItem("cb1");
+        cb_item_2 = new JCheckBoxMenuItem("cb2");
         
         bg = new ButtonGroup();
         
@@ -87,10 +104,18 @@ public class MenuStuffs extends JFrame {
             }
         });
         
+        reg_item_1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("regular item 1");
+            }
+        });
+        
         /////////////////////////////////////////////////////////
         
         menubar.add(reg_items);
         menubar.add(rb_items);
+        menubar.add(cb_items);
         menubar.add(popMenu);
         
         
@@ -103,6 +128,9 @@ public class MenuStuffs extends JFrame {
         rb_items.add(rb_item_2);
         rb_items.addSeparator();
         rb_items.add(rb_item_3);
+        
+        cb_items.add(cb_item_1);
+        cb_items.add(cb_item_2);
         
         bg.add(rb_item_1);
         bg.add(rb_item_2);
